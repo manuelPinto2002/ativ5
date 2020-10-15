05e5fb84ab4c085673562496e95b200f43d3523e
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-15 12:07:37 +0100

    Update tres.php and um.php

--- tres.php
+++ tres.php
@@ -7,4 +7,17 @@ $num2= "20";
 echo $txt1. " " .$txt2. " " .$num1. ". " .$txt3. $num2. "valores.";
 
 echo "<br>Manuel numero º5 12i-2";
+
+echo "<br><br>Os testes estão no nosso disco de rede em" ;
+echo " \\Curso\\Disciplinas\\Modulos\\Testes\\";
+echo "<br> <br>Em PHP, a declaração de uma variável implica a precedência de um carácter especial: o carater $.";
+echo "<br><br> O professor chegou perto do aluno e exclamou: \"se não estudares, vais reprovar de ano!\".";
+
+echo "<br><br>O professor
+<br>chegou perto
+<br>do aluno e
+<br>exclamou: \"se não
+<br>estudares, vais
+<br>reprovar de ano!\"";
+
 ?>
--- um.php
+++ um.php
@@ -2,8 +2,8 @@
 echo "guns n´ roses";
 ?>
 <?php //queres imprimir c:\xampp\
-echo "c:\\xampp\\";
+echo "<br>c:\\xampp\\";
 ?>
 <?php //queres imprimir "ser poeta é ser mais alto..."
-echo "\"ser poeta é ser mais alto...\"";
+echo "<br>\"ser poeta é ser mais alto...\"";
 ?>
